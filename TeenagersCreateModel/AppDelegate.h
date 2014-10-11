36adef733d43bb972454fd3c07489b4e634771c7
mac <mac@macdeMacBook-Pro.local>
2014-10-11 19:27:39 +0800

    Initial Commit

---
+++ TeenagersCreateModel/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  TeenagersCreateModel
+//
+//  Created by mac on 14-10-11.
+//  Copyright (c) 2014年 mac. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ TeenagersCreateModel/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  TeenagersCreateModel
+//
+//  Created by mac on 14-10-11.
+//  Copyright (c) 2014年 mac. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
